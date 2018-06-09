efa5e4fd08394e5bb8ddfe5ffb02ec1c8e34e5be
nureke06 <anefiyayev06@gmail.com>
2018-06-09 23:48:32 +0600

    Initial commit

---
+++ app/src/main/java/com/example/nurda/linechart/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.nurda.linechart;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {//} implements OnChartGestureListener, OnChartValueSelectedListener{
+
+    private static final String TAG = "MainActivity";
+
+    private LineChart mChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mChart = findViewById(R.id.lineChart);
+
+        // mChart.setOnChartGestureListener(MainActivity.this);
+        // mChart.setOnChartValueSelectedListener(MainActivity.this);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues = new ArrayList<>();
+
+        yValues.add(new Entry(0, 60f));
+        yValues.add(new Entry(1, 50f));
+        yValues.add(new Entry(2, 70f));
+        yValues.add(new Entry(3, 30f));
+        yValues.add(new Entry(4, 50f));
+        yValues.add(new Entry(5, 60f));
+        yValues.add(new Entry(6, 65f));
+
+        LineDataSet set1 = new LineDataSet(yValues,"Data set 1");
+        set1.setFillAlpha(100);
+        set1.setColor(Color.RED);
+        set1.setLineWidth(3f);
+        set1.setValueTextSize(15f);
+        set1.setValueTextColor(Color.BLUE);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        mChart.setData(data);
+
+
+    }
+
+}
